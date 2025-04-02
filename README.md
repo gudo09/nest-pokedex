23a8a40770d91ce4d42c4f83cb04d6d0a10f6a2c
Franco Gudiño <francogudino12@gmail.com>
2025-04-01 21:28:46 -0300

    endpoint seed optimizado

--- README.md
+++ README.md
@@ -6,19 +6,30 @@
 
 1. Clonar el repoistorio
 2. Ejecutar:
+
 ```
 npm install
 ```
+
 3. Tener Nest Cli instalado
+
 ```
 npm i -g @nestjs/cli
 ```
 
 4. Levantar la base de datos
+
 ```
 docker-compose up -d
 ```
 
+5. Ejecutar el endpoint seed para rellenar la base de datos
+
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack utilizado
-* NestJS
-* MongoDB
+
+- NestJS
+- MongoDB
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -9,7 +9,7 @@ import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
-  baseURL = 'https://pokeapi.co/api/v2/pokemon?limit=85';
+  baseURL = 'https://pokeapi.co/api/v2/pokemon?limit=385';
 
   constructor(
     private readonly httpService: HttpService,
@@ -19,17 +19,46 @@ export class SeedService {
   ) {}
 
   async runSeed() {
+    await this.pokemonModel.deleteMany({}); // borro toda la info que tengo en la coleccion antes de generar la seed
+
     const { data } = await this.httpService.axiosRef.get<PokeResponse>(
       this.baseURL,
     );
+    // ----------------------------------------------------------------
 
+    /*  // Método 1- Esta manera no es eficiente porque espera por cada iteracion del forof a que la promesa se resuelva
     for (const { name, url } of data.results) { // Foreach no puede manejar promesas. For of sí puede hacerlo
       const segments = url.split('/');
-
       const no: number = +segments[segments.length - 2];
-
       await this.pokemonModel.create({ name, no });
-    }
+    } */
+
+    // ----------------------------------------------------------------
+    /*  // Método 2 - Es más rápido que el metodo 1 pero sigo teniendo el problema que hago muchas consultas a la base de datos
+    const insertPromiseArray: Promise<Pokemon>[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      insertPromiseArray.push(this.pokemonModel.create({ no, name }));
+    });
+
+    await Promise.all(insertPromiseArray); //Espero que el arreglo de Promesas se resuelva */
+
+    // ----------------------------------------------------------------
+
+    // Método 3 - Una sola consulta para insertar muchos documentos
+    const pokemonToInsert: { no: number; name: string }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      pokemonToInsert.push({ no, name });
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    // ----------------------------------------------------------------
+
     return 'Seed executed';
   }
 }
